ebcf2874db2b3b01c369ad9023d1ce65258161e1
y.krylov <shadrus@gmail.com>
2020-08-28 09:59:31 +0300

    new Request

--- rabbitbus/manager.py
+++ rabbitbus/manager.py
@@ -4,7 +4,7 @@ import re
 import uuid
 import logging
 from time import sleep
-from typing import Dict, Coroutine, List
+from typing import Dict, Coroutine, Tuple, Type, Awaitable, Callable
 
 import aioamqp
 from aioamqp.channel import Channel
@@ -16,7 +16,13 @@ logger = logging.getLogger(__name__)
 
 class Configuration:
 
-    def __init__(self, rabbit_host, sleep_period, rabbit_username, rabbit_password, virtualhost, exchange_name, queue_name):
+    def __init__(self, rabbit_host,
+                 sleep_period,
+                 rabbit_username,
+                 rabbit_password,
+                 virtualhost,
+                 exchange_name,
+                 queue_name):
         self.rabbit_host = rabbit_host
         self.sleep_period = sleep_period
         self.rabbit_username = rabbit_username
@@ -32,34 +38,45 @@ class CorrelationManager:
         raise NotImplementedError('You should implement this yourself')
 
 
+class Request:
+    
+    def __init__(self, channel: Channel, body, envelope: Envelope, properties: Properties):
+        self.body = body
+        self.channel = channel
+        self.envelope = envelope
+        self.properties = properties
+    
+    def json(self):
+        return json.loads(self.body)
+
+
 class RouteManager:
     routes = {}
 
     def add_route(self, routing_key, view):
         self.routes[re.compile(routing_key)] = view
 
-    def _test_reg(self, routing_key):
+    def get_view(self, routing_key: str) -> Callable[[Request], Awaitable[None]]:
         for route, view in self.routes.items():
             if re.match(route, routing_key):
                 return view
 
-    def get_view(self, routing_key: str):
-        return self._test_reg(routing_key)
-
 
 class DatabusApp:
-    def __init__(self, conf: Configuration, correlation_manager: CorrelationManager = None, max_workers=10):
+    def __init__(self,
+                 conf: Configuration,
+                 correlation_manager: Type[CorrelationManager] = None,
+                 max_workers: int = 10):
         if correlation_manager:
             self.correlation_manager = correlation_manager()
         else:
             self.correlation_manager = None
-        self.conf = conf
+        self.conf: Configuration = conf
         self.router = RouteManager()
-        self.task_queue = asyncio.Queue()
+        self.task_queue: asyncio.Queue[Tuple[Callable[[Request], Awaitable[None]], Request]] = asyncio.Queue()
         self.max_workers = max_workers
 
-    def add_route(self, routing_key, view, as_list=False):
-        view.as_list = as_list
+    def add_route(self, routing_key, view):
         self.router.add_route(routing_key, view)
 
     def add_routes(self, routes: Dict[str, Coroutine[Dict, None, None]]):
@@ -73,10 +90,6 @@ class DatabusApp:
         else:
             logger.warning(f"Got unknown correlation_id: {correlation_id}")
 
-    async def list_to_view(self, func, data: List[Dict]):
-        [await func(item) for item in data]
-        return True
-
     async def _serve_message(self, channel: Channel, body, envelope: Envelope, properties: Properties):
         try:
             if envelope.routing_key == self.conf.queue_name and self.correlation_manager:
@@ -84,17 +97,20 @@ class DatabusApp:
             else:
                 routing_key = envelope.routing_key
             work_func = self.router.get_view(routing_key)
-            await self.task_queue.put((work_func, json.loads(body), channel, envelope.delivery_tag))
+            request = Request(channel, body, envelope, properties)
+            await self.task_queue.put((work_func, request))
         except ConnectionResetError as ex:
             logger.exception(ex)
             raise ex
 
-    async def _receive(self, reconnect: bool = True, reconnect_wait_period=2):
+    async def _receive(self, reconnect: bool = True, reconnect_wait_period: int = 2):
         async def on_error_callback(exception):
             logger.debug('on_error_callback: %s', str(exception))
             if isinstance(exception, aioamqp.AmqpClosedConnection):
                 await asyncio.sleep(reconnect_wait_period)
-                await self._receive(self.max_workers)
+                await self._receive(reconnect, reconnect_wait_period)
+
+        transport = None
         try:
             transport, protocol = await aioamqp.connect(on_error=on_error_callback,
                                                         host=self.conf.rabbit_host,
@@ -107,45 +123,45 @@ class DatabusApp:
         except aioamqp.AmqpClosedConnection:
             logging.debug("AmqpClosedConnection, will call on_error")
         except (OSError, ConnectionRefusedError) as e:
-            if not transport.is_closing():
+            if transport and not transport.is_closing():
                 await transport.close()
             if reconnect:
                 logger.warning(str(e))
                 await asyncio.sleep(reconnect_wait_period)
-                await self._receive()
+                await self._receive(reconnect, reconnect_wait_period)
             else:
                 raise e
 
-    async def _consume(self, worker_id):
+    async def __set_response(self, request: Request, success, worker_id):
+        if success:
+            if request.envelope.delivery_tag:
+                await request.channel.basic_client_ack(delivery_tag=request.envelope.delivery_tag)
+            logger.debug("Worker %s completed the task" % worker_id)
+        else:
+            logger.warning("Router handler %s doesn't returned anything for request %s" % (request.body, worker_id))
+            if request.envelope.delivery_tag:
+                await request.channel.basic_client_nack(delivery_tag=request.envelope.delivery_tag)
+            await asyncio.sleep(10)
+
+    async def _consume(self):
+        worker_id = str(uuid.uuid4())
         while True:
             # wait for an item from the producer
-            func, item, channel, delivery_tag = await self.task_queue.get()
+            func, request = await self.task_queue.get()
             try:
                 logger.debug("Worker %s started" % worker_id)
-                if func.as_list is True:
-                    result = await self.list_to_view(func, item)
-                else:
-                    result = await func(item)
-                if result:
-                    if delivery_tag:
-                        await channel.basic_client_ack(delivery_tag=delivery_tag)
-                    logger.debug("Worker %s completed the task" % worker_id)
-                else:
-                    logger.warning("Router handler %s doesn't returned anything for worker %s" % (func, worker_id))
-                    if delivery_tag:
-                        await channel.basic_client_nack(delivery_tag=delivery_tag)
-                    await asyncio.sleep(60)
+                await self.__set_response(request, await func(request))
                 self.task_queue.task_done()
             except Exception as ex:
                 logger.exception(ex)
-                logger.warning("Worker %s going to sleep" % worker_id)
-                if delivery_tag:
-                    await channel.basic_client_nack(delivery_tag=delivery_tag)
-                await asyncio.sleep(60)
+                logger.warning("Worker %s going to sleep because of exception" % worker_id)
+                if request.envelope.delivery_tag:
+                    await request.channel.basic_client_nack(delivery_tag=request.envelope.delivery_tag)
+                await asyncio.sleep(10)
             await asyncio.sleep(0.01)
 
     async def _create_workers(self):
-        return [asyncio.ensure_future(self._consume(str(uuid.uuid4()))) for worker in range(self.max_workers)]
+        return [asyncio.create_task(self._consume() for _ in range(self.max_workers))]
 
     def start(self, loop):
         workers = []
